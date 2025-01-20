98a4a31dd1aa79a8e6889e36ec84b3b93a6de8b1
Costantino <xtremexspc@gmail.com>
2025-01-20 16:13:54 +0100

    Completed Stow initial setup. All configuration files organized.

---
+++ nvim/.config/nvim/lua/plugins/lazyvim.lua
@@ -0,0 +1,12 @@
+return {
+    "LazyVim/LazyVim",
+    opts = {
+        format = {
+            enabled = false, -- Disabilita la formattazione globale
+            formatters_by_ft = {
+                c = { "clang_format_custom" }, 
+                cpp = { "clang_format_custom" },
+            },
+        },
+    },
+}
