cc5c716b1d485c57dc589e9d5d700644d9115df1
LCS-Dev.Mac <xtremexspc@gmail.com>
2025-09-23 17:24:21 +0200

    Enhance AeroSpace and SketchyBar integration: update workspace change handling, improve app icon management, and refine layout configurations.

--- sketchybar/.config/sketchybar/plugins/aerospace.sh
+++ sketchybar/.config/sketchybar/plugins/aerospace.sh
@@ -1,9 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# ~/.config/sketchybar/plugins/aerospace.sh
+# Debug of variables passed to the script:
+echo "Script called with: NAME=$NAME, FOCUSED_WORKSPACE=$FOCUSED_WORKSPACE, Arg1=$1" >>/tmp/aerospace-debug.log
 
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-    sketchybar --set $NAME background.drawing=on
+    sketchybar --set $NAME background.drawing=on \
+        background.color=0xff007acc \
+        label.color=0xffffffff
 else
-    sketchybar --set $NAME background.drawing=off
+    sketchybar --set $NAME background.drawing=off \
+        background.color=0x00000000 \
+        label.color=0xff888888
 fi
