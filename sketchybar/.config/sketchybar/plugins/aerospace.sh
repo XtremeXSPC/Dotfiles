60d981d596bcbd6340e329f34c7b911d27450007
LCS-Dev.Mac <xtremexspc@gmail.com>
2025-09-23 12:43:51 +0200

    Refactor aerospace plugin: streamline workspace indicator creation and improve script readability.

--- sketchybar/.config/sketchybar/plugins/aerospace.sh
+++ sketchybar/.config/sketchybar/plugins/aerospace.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
+
 # ~/.config/sketchybar/plugins/aerospace.sh
 
-source "$CONFIG_DIR/colors.sh"
-
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-    sketchybar --set space.$1 background.drawing=on \
-        label.color="$BAR_COLOR" \
-        background.color="$ACCENT_COLOR"
+    sketchybar --set $NAME background.drawing=on
 else
-    sketchybar --set space.$1 background.drawing=off \
-        label.color="$WHITE"
+    sketchybar --set $NAME background.drawing=off
 fi
