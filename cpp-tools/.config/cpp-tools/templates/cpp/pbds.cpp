2cf01d57d36a8bab9c42011e5d77872942eef8d7
LCS-Dev.Mac <xtremexspc@gmail.com>
2025-11-23 12:58:27 +0100

    refactor(cpp-tools): Wrap template type aliases in header guards.
    - Enclose the fundamental, extended-precision, and container type aliases in `#ifndef __TYPES__` guards in advanced.cpp, default.cpp, and pbds.cpp.
    - Prevent redefinition errors when multiple templates or headers are combined.

--- cpp-tools/.config/cpp-tools/templates/cpp/pbds.cpp
+++ cpp-tools/.config/cpp-tools/templates/cpp/pbds.cpp
@@ -44,84 +44,97 @@
 //===----------------------------------------------------------------------===//
 /* Type Aliases and Constants */
 
-// Fundamental type aliases with explicit sizes:
-using I8   = std::int8_t;
-using I16  = std::int16_t;
-using I32  = std::int32_t;
-using I64  = std::int64_t;
-using U8   = std::uint8_t;
-using U16  = std::uint16_t;
-using U32  = std::uint32_t;
-using U64  = std::uint64_t;
-using F32  = float;
-using F64  = double;
-using F80  = long double;
+#ifndef __TYPES__
+#define __TYPES__
 
-// Extended precision types:
-#ifdef __SIZEOF_INT128__
+  // Fundamental type aliases with explicit sizes:
+  using I8   = std::int8_t;
+  using I16  = std::int16_t;
+  using I32  = std::int32_t;
+  using I64  = std::int64_t;
+  using U8   = std::uint8_t;
+  using U16  = std::uint16_t;
+  using U32  = std::uint32_t;
+  using U64  = std::uint64_t;
+  using F32  = float;
+  using F64  = double;
+  using F80  = long double;
+
+  // Extended precision types:
+  #ifdef __SIZEOF_INT128__
   using I128 = __int128;
   using U128 = unsigned __int128;
-#else
+  #else
   using I128 = std::int64_t;
   using U128 = std::uint64_t;
-#endif
+  #endif
 
-#ifdef __FLOAT128__
+  #ifdef __FLOAT128__
   using F128 = __float128;
-#else
+  #else
   using F128 = long double;
-#endif
+  #endif
 
-// Legacy aliases for backward compatibility:
-using ll  = I64;
-using ull = U64;
-using ld  = F80;
+  // Legacy aliases for backward compatibility:
+  using ll  = I64;
+  using ull = U64;
+  using ld  = F80;
 
-// Container type aliases:
-template <class T>
-using VC   = std::vector<T>;
-template <class T>
-using VVC  = VC<VC<T>>;
-template <class T>
-using VVVC = VC<VVC<T>>;
-template <class T>
-using VVVVC = VC<VVVC<T>>;
+  // Container type aliases:
+  template <class T>
+  using VC   = std::vector<T>;
+  template <class T>
+  using VVC  = VC<VC<T>>;
+  template <class T>
+  using VVVC = VC<VVC<T>>;
+  template <class T>
+  using VVVVC = VC<VVVC<T>>;
 
-// Specialized container aliases:
-using VI    = VC<I64>;
-using VVI   = VVC<I64>;
-using VVVI  = VVVC<I64>;
-using VB    = VC<bool>;
-using VS    = VC<std::string>;
-using VU8   = VC<U8>;
-using VU32  = VC<U32>;
-using VU64  = VC<U64>;
+  // Specialized container aliases:
+  using VI    = VC<I64>;
+  using VVI   = VVC<I64>;
+  using VVVI  = VVVC<I64>;
+  using VB    = VC<bool>;
+  using VS    = VC<std::string>;
+  using VU8   = VC<U8>;
+  using VU32  = VC<U32>;
+  using VU64  = VC<U64>;
 
-// Pair and tuple aliases:
-using PII = std::pair<I32, I32>;
-using PLL = std::pair<I64, I64>;
-using PLD = std::pair<ld, ld>;
-template <class T, class U>
-using P = std::pair<T, U>;
+  // Queue and stack aliases:
+  template <class T>
+  using Deque = std::deque<T>;
+  template <class T>
+  using Stack = std::stack<T, std::deque<T>>;
+  template <class T>
+  using Queue = std::queue<T, std::deque<T>>;
 
-using VPII = VC<PII>;
-using VPLL = VC<PLL>;
-template <class T, class U>
-using VP = VC<P<T, U>>;
+  // Pair and tuple aliases:
+  using PII = std::pair<I32, I32>;
+  using PLL = std::pair<I64, I64>;
+  using PLD = std::pair<ld, ld>;
+  template <class T, class U>
+  using P = std::pair<T, U>;
 
-// Priority queue aliases:
-template <class T>
-using PQ_max = std::priority_queue<T>;
-template <class T>
-using PQ_min = std::priority_queue<T, VC<T>, std::greater<T>>;
+  using VPII = VC<PII>;
+  using VPLL = VC<PLL>;
+  template <class T, class U>
+  using VP = VC<P<T, U>>;
 
-// Advanced container aliases:
-template <class K, class V>
-using UMap = std::unordered_map<K, V>;
-template <class T>
-using USet = std::unordered_set<T>;
-template <class T>
-using MSet = std::multiset<T>;
+  // Priority queue aliases:
+  template <class T>
+  using PQ_max = std::priority_queue<T>;
+  template <class T>
+  using PQ_min = std::priority_queue<T, VC<T>, std::greater<T>>;
+
+  // Hash-based containers:
+  template <class K, class V>
+  using UMap = std::unordered_map<K, V>;
+  template <class T>
+  using USet = std::unordered_set<T>;
+  template <class T>
+  using MSet = std::multiset<T>;
+
+#endif // __TYPES__
 
 // Policy-based data structures:
 using namespace __gnu_pbds;
@@ -139,6 +152,20 @@ constexpr long long   LINF = 0x3f3f3f3f3f3f3f3fLL;
 constexpr int         LIM  = 1000000 + 5;
 constexpr int         MOD  = 1000000007;
 constexpr int         MOD2 = 998244353;
+
+// Advanced FOR loop system:
+#define FOR1(a) for (I64 _ = 0; _ < (a); ++_)
+#define FOR2(i, a) for (I64 i = 0; i < (a); ++i)
+#define FOR3(i, a, b) for (I64 i = (a); i < (b); ++i)
+#define FOR4(i, a, b, c) for (I64 i = (a); i < (b); i += (c))
+#define FOR1_R(a) for (I64 i = (a) - 1; i >= 0; --i)
+#define FOR2_R(i, a) for (I64 i = (a) - 1; i >= 0; --i)
+#define FOR3_R(i, a, b) for (I64 i = (b) - 1; i >= (a); --i)
+
+#define overload4(a, b, c, d, e, ...) e
+#define overload3(a, b, c, d, ...) d
+#define FOR(...) overload4(__VA_ARGS__, FOR4, FOR3, FOR2, FOR1)(__VA_ARGS__)
+#define FOR_R(...) overload3(__VA_ARGS__, FOR3_R, FOR2_R, FOR1_R)(__VA_ARGS__)
 
 using namespace std;
 
