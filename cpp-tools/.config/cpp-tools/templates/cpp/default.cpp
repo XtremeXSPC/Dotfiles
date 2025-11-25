5cad29e6770460dd25a6adff6b6ed162a64f6d2b
LCS-Dev.Mac <xtremexspc@gmail.com>
2025-11-25 23:29:48 +0100

    refactor(cpp-tools): Tidy up type alias formatting in default.cpp template.
    - Realign type alias declarations for consistent column spacing.
    - Add LL, ULL, and LD legacy alias shorthands alongside the existing ll/ull/ld names.

--- cpp-tools/.config/cpp-tools/templates/cpp/default.cpp
+++ cpp-tools/.config/cpp-tools/templates/cpp/default.cpp
@@ -43,59 +43,62 @@
 
 #ifndef __TYPES__
 #define __TYPES__
-
   // Fundamental type aliases with explicit sizes:
-  using I8   = std::int8_t;
-  using I16  = std::int16_t;
-  using I32  = std::int32_t;
-  using I64  = std::int64_t;
-  using U8   = std::uint8_t;
-  using U16  = std::uint16_t;
-  using U32  = std::uint32_t;
-  using U64  = std::uint64_t;
-  using F32  = float;
-  using F64  = double;
-  using F80  = long double;
+  using I8  = std::int8_t;
+  using I16 = std::int16_t;
+  using I32 = std::int32_t;
+  using I64 = std::int64_t;
+  using U8  = std::uint8_t;
+  using U16 = std::uint16_t;
+  using U32 = std::uint32_t;
+  using U64 = std::uint64_t;
+  using F32 = float;
+  using F64 = double;
+  using F80 = long double;
+
+  using LL  = I64;
+  using ULL = U64;
+  using LD  = F80;
 
   // Extended precision types:
   #ifdef __SIZEOF_INT128__
-  using I128 = __int128;
-  using U128 = unsigned __int128;
+    using I128 = __int128;
+    using U128 = unsigned __int128;
   #else
-  using I128 = std::int64_t;
-  using U128 = std::uint64_t;
-  #endif
-
-  #ifdef __FLOAT128__
-  using F128 = __float128;
-  #else
-  using F128 = long double;
+    using I128 = std::int64_t;
+    using U128 = std::uint64_t;
   #endif
 
   // Legacy aliases for backward compatibility:
-  using ll  = I64;
-  using ull = U64;
-  using ld  = F80;
+  #ifdef __FLOAT128__
+    using F128 = __float128;
+  #else
+    using F128 = long double;
+  #endif
 
   // Container type aliases:
   template <class T>
-  using VC   = std::vector<T>;
+  using VC = std::vector<T>;
   template <class T>
-  using VVC  = VC<VC<T>>;
+  using VVC = VC<VC<T>>;
   template <class T>
   using VVVC = VC<VVC<T>>;
   template <class T>
   using VVVVC = VC<VVVC<T>>;
 
   // Specialized container aliases:
-  using VI    = VC<I64>;
-  using VVI   = VVC<I64>;
-  using VVVI  = VVVC<I64>;
-  using VB    = VC<bool>;
-  using VS    = VC<std::string>;
-  using VU8   = VC<U8>;
-  using VU32  = VC<U32>;
-  using VU64  = VC<U64>;
+  using VI   = VC<I32>;
+  using VVI  = VVC<I32>;
+  using VVVI = VVVC<I32>;
+  using VL   = VC<I64>;
+  using VVL  = VVC<I64>;
+  using VVVL = VVVC<I64>;
+  using VB   = VC<bool>;
+  using VS   = VC<std::string>;
+  using VU8  = VC<U8>;
+  using VU32 = VC<U32>;
+  using VU64 = VC<U64>;
+  using VF   = VC<F64>;
 
   // Queue and stack aliases:
   template <class T>
@@ -106,32 +109,31 @@
   using Queue = std::queue<T, std::deque<T>>;
 
   // Pair and tuple aliases:
+  template <class T, class U>
+  using P = std::pair<T, U>;
   using PII = std::pair<I32, I32>;
   using PLL = std::pair<I64, I64>;
   using PLD = std::pair<ld, ld>;
-  template <class T, class U>
-  using P = std::pair<T, U>;
 
-  using VPII = VC<PII>;
-  using VPLL = VC<PLL>;
   template <class T, class U>
   using VP = VC<P<T, U>>;
+  using VPII = VC<PII>;
+  using VPLL = VC<PLL>;
 
-  // Priority queue aliases:
+  // Priority queue aliases
   template <class T>
   using PQ_max = std::priority_queue<T>;
   template <class T>
   using PQ_min = std::priority_queue<T, VC<T>, std::greater<T>>;
 
-  // Hash-based containers:
+  // Map and set aliases:
   template <class K, class V>
   using UMap = std::unordered_map<K, V>;
   template <class T>
   using USet = std::unordered_set<T>;
   template <class T>
   using MSet = std::multiset<T>;
-
-#endif // __TYPES__
+#endif  // __TYPES__
 
 // Mathematical constants:
 constexpr long double PI   = 3.141592653589793238462643383279502884L;
@@ -158,6 +160,7 @@ constexpr int         MOD2 = 998244353;
 #define FOR_R(...) overload3(__VA_ARGS__, FOR3_R, FOR2_R, FOR1_R)(__VA_ARGS__)
 
 using namespace std;
+// clang-format on
 
 //===----------------------------------------------------------------------===//
 /* Data Types and Function Definitions */
