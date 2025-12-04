746fea1baa11496c4237c3a9ea52a0bd37a7810d
LCS-Dev.Mac <xtremexspc@gmail.com>
2025-12-04 15:04:59 +0100

    style(nvim): Improve comments for clarity in lang-nix-disable.lua

--- nvim/.config/nvim/lua/plugins/lang-nix-disable.lua
+++ nvim/.config/nvim/lua/plugins/lang-nix-disable.lua
@@ -5,7 +5,7 @@ local uname = vim.loop.os_uname().sysname
 
 if uname == "Linux" then
   return {
-    -- Stop LSP setup for nil_ls
+    -- Stop LSP setup for "nil_ls".
     {
       "neovim/nvim-lspconfig",
       opts = {
@@ -14,11 +14,11 @@ if uname == "Linux" then
         },
       },
     },
-    -- Remove nil from Mason's ensure_installed list (if present)
+    -- Remove nil from Mason's ensure_installed list (if present).
     {
       "williamboman/mason.nvim",
       opts = function(_, opts)
-        local skip = { nil = true, nil_ls = true }
+        local skip = { ["nil"] = true, nil_ls = true }
         opts.ensure_installed = vim.tbl_filter(function(pkg)
           return not skip[pkg]
         end, opts.ensure_installed or {})
