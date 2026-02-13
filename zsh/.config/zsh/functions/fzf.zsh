51f2b81f86ff295fff6bcf44ef8213493c85b235
LCS-Dev.Mac <xtremexspc@gmail.com>
2026-02-13 17:58:59 +0100

    chore: Add end comments to various zsh function files for clarity.

--- zsh/.config/zsh/functions/fzf.zsh
+++ zsh/.config/zsh/functions/fzf.zsh
@@ -162,3 +162,4 @@ alias ffec='_fuzzy_edit_search_file_content'
 alias ffch='_fuzzy_search_cmd_history'
 
 # ============================================================================ #
+# End of fzf.zsh
